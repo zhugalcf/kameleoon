220047062b94ba86915f63e5e23af0ee6ba72e89
zhugalcf <zhugalcf@gmail.com>
2023-12-06 15:16:14 +0300

    Create project

---
+++ src/main/java/com/zhugalcf/kameleoon/KameleoonApplication.java
@@ -0,0 +1,13 @@
+package com.zhugalcf.kameleoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KameleoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KameleoonApplication.class, args);
+	}
+
+}
